fddb670c544cbc92b6c3ca38fd01693bb0e45a72
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-03-13 21:28:15 +0500

    many

--- AppNavigation/ExaminationTab.js
+++ AppNavigation/ExaminationTab.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import { View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 // Screens import for main screen
 import DateSheetMain from "../screens/ExaminationScreen/DateSheetMain";
@@ -13,9 +17,32 @@ const tab = createBottomTabNavigator();
 
 // Screen stacks
 
-const SyllabusStack = () => {
+const HeaderLeft = () => {
+  const navigation = useNavigation();
   return (
-    <stack.Navigator>
+    <View style={{ marginLeft: 10 }}>
+      <MaterialIcons
+        name="menu"
+        size={24}
+        onPress={() => {
+          navigation.openDrawer();
+        }}
+      />
+    </View>
+  );
+};
+
+const SyllabusStack = () => {
+  navigation = useNavigation();
+  return (
+    <stack.Navigator
+      screenOptions={{
+        headerLeft: () => {
+          return <HeaderLeft />;
+        },
+        headerTitleAlign: "center",
+      }}
+    >
       <stack.Screen name="SyllabusMain" component={SyllabusMain} />
       <stack.Screen name="Syllabus" component={Syllabus} />
     </stack.Navigator>
@@ -24,7 +51,14 @@ const SyllabusStack = () => {
 
 const DateSheetStack = () => {
   return (
-    <stack.Navigator>
+    <stack.Navigator
+      screenOptions={{
+        headerLeft: () => {
+          return <HeaderLeft />;
+        },
+        headerTitleAlign: "center",
+      }}
+    >
       <stack.Screen name="DateSheetMain" component={DateSheetMain} />
       <stack.Screen name="DateSheet" component={DateSheet} />
     </stack.Navigator>
@@ -33,7 +67,22 @@ const DateSheetStack = () => {
 
 const ExaminationTab = () => {
   return (
-    <tab.Navigator>
+    <tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Syllabus") {
+            iconName = "newspaper-sharp";
+          } else if (route.name === "DateSheet") {
+            iconName = "ios-calendar";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={24} color="black" />;
+        },
+      })}
+    >
       <tab.Screen name="Syllabus" component={SyllabusStack} />
       <tab.Screen name="DateSheet" component={DateSheetStack} />
     </tab.Navigator>
