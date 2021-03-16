ae9ef81daa608d046a688ba5b54e8a260d2802f6
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-03-16 21:54:44 +0500

    alot

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Navigation from "./AppNavigation/MainNavigation";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
